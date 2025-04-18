7a31fc920d0e7eb62c5b9e96458988656e6c8d65
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-04-18 16:02:47 +0530

    first commit

---
+++ fleetManagementSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fleetManagementSystem
+//
+//  Created by Jai Khurana on 18/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+		var body: some View {
+				VStack {
+						Image(systemName: "figure")
+								.imageScale(.large)
+								.foregroundStyle(.tint)
+						Text("Hello, world!")
+				}
+				.padding()
+		}
+}
+
+#Preview {
+		ContentView()
+}
---
+++ fleetManagementSystem/fleetManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  fleetManagementSystemApp.swift
+//  fleetManagementSystem
+//
+//  Created by Jai Khurana on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct fleetManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
