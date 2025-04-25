2f68dd65e8a7434b6e3b2954b10a496dc2461b71
Bhumika Sharma <130481601+bhumika1331@users.noreply.github.com>
2025-04-25 10:02:45 +0530

    Added Data Model
    New Data Model, Firebase manager added.

--- fleetManagementSystem/fleetManagementSystemApp.swift
+++ fleetManagementSystem/fleetManagementSystemApp.swift
@@ -12,7 +12,12 @@ import FirebaseCore
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+
+        // let dbService = FleetDriverDBService()
+        // dbService.uploadSampleFleetDrivers(sampleDrivers)
+
         return true
+
     }
 }
 
